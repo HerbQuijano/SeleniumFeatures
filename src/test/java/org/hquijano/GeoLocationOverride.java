16fba6729d8c89d5027b99ee864d0659ccc159a7
hquijano <herbert.quijano@gmail.com>
2022-01-22 11:03:44 -0600

    Geolocation Selenium 4 - Chrome Dev Protocol

--- src/test/java/org/hquijano/GeoLocationOverride.java
+++ src/test/java/org/hquijano/GeoLocationOverride.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.v96.emulation.Emulation;
 
 import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 
@@ -17,7 +19,14 @@ public class GeoLocationOverride {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
         devTools.createSession();
-        devTools.send(Emulation.setGeolocationOverride(Optional.of(20.481047), Optional.of(-86.971985), Optional.of(10)));
+
+        Map<String, Object> geolocation = new HashMap<String, Object>();
+        geolocation.put("latitude", 40.730610);
+        geolocation.put("longitude", -73.935242);
+        geolocation.put("accuracy", 5);
+
+        //devTools.send(Emulation.setGeolocationOverride(Optional.of(20.481047), Optional.of(-86.971985), Optional.of(10)));
+        driver.executeCdpCommand("Emulation.setGeolocationOverride", geolocation);
 
         driver.get("https://www.google.com/maps");
         driver.findElement(By.cssSelector("#pWhrzc-mylocation")).click();
